86e980d53c712f70eb3416088759cfabbfe1f501
waamber <amberastuart@aol.com>
2018-01-27 11:46:13 -0600

    added department classes

---
+++ Architecture/DepartmentBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architecture
+{
+    class DepartmentBase
+    {
+        public string DeptName { get; set; }
+        public string Supervisor { get; set; }
+        public int EmployeeCount { get; set; }
+    }
+}
